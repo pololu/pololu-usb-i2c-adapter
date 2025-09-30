fed10431807ac83fd3a5dff468f34c0131fdb05e
David (Pololu) <dev-david@pololu.com>
2025-09-29 19:25:37 -0700

    default.nix: Filter out build directories in the firmware directory.
    They waste space and also can confuse CMake.

--- default.nix
+++ default.nix
@@ -4,7 +4,9 @@ let
 in
   pkgs.stdenv.mkDerivation rec {
     name = "pololu-usb-to-i2c-adapter-firmware";
-    src = ./firmware;
+    src = builtins.filterSource
+      (path: type: (builtins.substring 0 5 (baseNameOf path)) != "build")
+      ./firmware;
     buildInputs = [ pkgs.gcc-arm-embedded pkgs.cmake pkgs.ninja ];
     builder = ./firmware/nix_builder.sh;
   }
